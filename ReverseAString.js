6546798769ddd60c1a9cb9e045d328dea598f930
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-09-04 19:13:01 -0700

    Add function to reverse a string

---
+++ ReverseAString.js
@@ -0,0 +1,9 @@
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+console.log(reverseString("Mars is Great!"));
+// "tpircSavaJ"
+
+console.log(reverseString("Warp Drive"));
+// "sraM"
