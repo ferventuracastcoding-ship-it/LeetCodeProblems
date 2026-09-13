be3850c63cca49074f6dfa5199a33f20544ae70a
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-09-12 18:26:15 -0700

    Refactor twoSum function for clarity and functionality
    Refactor twoSum function by uncommenting code and fixing variable name from 'compliment' to 'complement'.

--- TwoSum.js
+++ TwoSum.js
@@ -1,18 +1,21 @@
 function twoSum(nums, target) {
+    // const map = new Map();
     const map = new Map();
 
     for (let i = 0; i < nums.length; i++) {
+        // const compliment = target - nums[i];
         const complement = target - nums[i];
 
         if (map.has(complement)) {
+            // return [map.get(compliment), i];
             return [map.get(complement), i];
         }
-
+        // map.set(nums[i], i);
         map.set(nums[i], i);
     }
-
+ // return [];
     return [];
 }
-
+// console.log(twoSum([2,7,11,15], 9));
 console.log(twoSum([2, 7, 11, 15], 9));
 // [0, 1]
